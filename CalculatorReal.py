fd4ef20769003d3b6453b29c5f0dee35696f8382
hfgordon2019 <hfgordon2019@chadwickschool.org>
2016-10-31 10:20:22 -0700

    Im DONE!

--- CalculatorReal.py
+++ CalculatorReal.py
@@ -1,16 +1,16 @@
 from Myro import *
-
+print("welcome to the calculator program!")
 #Addition
 
 def add(num1, num2):
     result = num1;
     for i in range(0,num2):
-        print(i)
+      #  print(i)
         result = result +1
     return result
     
 answer = add(5,5)    
-#print(answer)
+print(answer)
 
 #Subtraction
 
@@ -22,16 +22,32 @@ def sub(num1, num2):
     return result
     
 answer = sub(18,15)    
-#print(answer)
+print(answer)
 
 #Multiplication
 
 def multiply(num1, num2):
     result = 0;
     for i in range(num2):
-        print(i)
+      #  print(i)
         result = add(result,num1);
     return result
   #  add result to num1 num2 times
 answer = multiply(10,5)    
 print(answer)
+
+#Division
+def divide(num1,num2):
+    result = 0
+    while num1>0:
+        num1 = sub(num1,num2);
+        result = result +1 
+    if num1<0 :
+        result = result -1
+    print (num1) 
+    return result 
+answer = divide (18,7)
+print(answer)
+
+
+
